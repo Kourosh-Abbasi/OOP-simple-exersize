a808702c2263dfb326db08c51e373f457f3fe8ce
Mona Ahmadi <seyedeh.mona.ahmadi.2005@gmail.com>
2025-03-08 17:53:54 +0330

    The simple exercise is done.

---
+++ Main.java
@@ -0,0 +1,11 @@
+
+public class Main {
+    public static void main(String[] args) {
+        Person person1 = new Person("Mahy" , 20);
+        Person person2 = new Person("Mona", 19);
+        person1.display();
+        person1.isadult();
+        person2.display();
+        person2.isadult();
+    }
+}
