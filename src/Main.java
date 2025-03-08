a35ad8b2deddd4cdf1fdc56edae58d963d0b3afb
AryaRameshgar <aria.rameshgar2024@gmail.com>
2025-03-08 20:33:25 +0330

    person

---
+++ src/Main.java
@@ -0,0 +1,11 @@
+//TIP To <b>Run</b> code, press <shortcut actionId="Run"/> or
+// click the <icon src="AllIcons.Actions.Execute"/> icon in the gutter.
+public class Main
+{
+    public static void main(String[] args)
+    {
+        Person me  = new Person("arya" , 19);
+        me.display();
+        me.check_age();
+    }
+}
